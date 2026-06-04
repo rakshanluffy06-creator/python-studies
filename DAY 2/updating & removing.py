22c310126bd7919f5da2424cc375484de593206a
rakshanluffy06-creator <rakshanluffy06@gmail.com>
2026-06-04 18:30:25 +0530

    Create updating & removing.py

---
+++ DAY 2/updating & removing.py
@@ -0,0 +1,15 @@
+updating:
+
+players = ["virat", "dhoni", "rohit"]
+players[1] = "hardik"
+print(players)
+
+removing:
+
+players = ["virat", "dhoni", "rohit"]
+
+players.remove("dhoni")
+print(players)
+
+players.pop(0)
+print(players)
