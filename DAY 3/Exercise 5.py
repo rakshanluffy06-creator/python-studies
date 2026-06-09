ea070f37900c1d1ff5d8207577da9bc695af1dff
rakshanluffy06-creator <rakshanluffy06@gmail.com>
2026-06-09 16:29:43 +0530

    Create Exercise 5.py

---
+++ DAY 3/Exercise 5.py
@@ -0,0 +1,9 @@
+player = {
+    "player_name": "Gohan",
+    "team": "Apollo Warriors",
+    "sport": "Cricket",
+    "coach": "Mr. rocky bhai"
+}
+
+for key, value in player.items():
+    print(key, ":", value)
